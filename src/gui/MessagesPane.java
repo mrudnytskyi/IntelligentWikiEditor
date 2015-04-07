5bb2d8109362811f18539bf3f39069018a6dd026
Myroslav Rudnytskyi <miroslav.rudnitsky@gmail.com>
2015-04-08 01:09:55 +0300

    Refactoring
    Added documentation for MessagesPane class

--- src/gui/MessagesPane.java
+++ src/gui/MessagesPane.java
@@ -16,6 +16,15 @@ import javax.swing.text.StyleConstants;
 import javax.swing.text.StyleContext;
 
 /**
+ * MessagesPane is a text component, created to show different types of 
+ * messages: errors, warnings and information messages. Every type is
+ * highlighted using color: for errors it is red, for warnings - blue and only
+ * information is not highlighted and painted using black color.
+ * <p>
+ * Every message has it's own time, which is displayed also.
+ * <p>
+ * Note, that this component use {@link JTextPane} for displaying messages and
+ * is subclass of {@link JPanel}.
  * 
  * @author Mir4ik
  * @version 0.1 06.04.2014
@@ -44,14 +53,29 @@ public class MessagesPane extends JPanel {
 		pane.setEditable(false);
 	}
 	
+	/**
+	 * Appends information message to pane.
+	 * 
+	 * @param text	message
+	 */
 	public void info(String text) {
 		append(text, null);
 	}
 	
+	/**
+	 * Appends error message to pane.
+	 *  
+	 * @param text	message
+	 */
 	public void error(String text) {
 		append(text, errorHighliter);
 	}
 	
+	/**
+	 * Appends warning message to pane.
+	 * 
+	 * @param text	message
+	 */
 	public void warn(String text) {
 		append(text, warnHighliter);
 	}
@@ -66,13 +90,12 @@ public class MessagesPane extends JPanel {
 		Document doc = pane.getStyledDocument();
 		try {
 			doc.insertString(doc.getLength(), dateText, highliter);
-		} catch (BadLocationException ignoredException) {}
+		} catch (BadLocationException ignoredException) {
+			// can not catch 
+		}
 	}
 	
-	private static String convertDate(long time) {
-		if (time < 0) {
-			throw new IllegalArgumentException("Time can not be negative!");
-		}
+	private String convertDate(long time) {
 		Calendar calendar = Calendar.getInstance();
 		calendar.setTime(new Date(time));
 		StringBuilder sb = new StringBuilder();
