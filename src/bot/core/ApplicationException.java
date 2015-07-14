af10b31a00840a60084ae64fc57476cd2260ed02
mrudnitsky <miroslav.rudnitsky@gmail.com>
2015-07-14 18:18:36 +0300

    Exceptions refactoring
    Now ApplicationException is unchecked exception

--- src/bot/core/ApplicationException.java
+++ src/bot/core/ApplicationException.java
@@ -14,26 +14,46 @@
  */
 package bot.core;
 
+import java.util.logging.Logger;
+
 /**
+ * Exception class, created to present wrong situations on application level.
+ * Typical usage of it is to wrap low-level exceptions.
+ * <p>
+ * Note, that it is <i>unchecked</i> exception, so, don't write it in the
+ * method's signature.
  * 
  * @author Myroslav Rudnytskyi
  * @version 0.1 29.08.2014
+ * @see RuntimeException
  */
-public class ApplicationException extends Exception {
+public class ApplicationException extends RuntimeException {
 
 	private static final long serialVersionUID = 3189658968868419110L;
 
-	public ApplicationException() {}
-	
+	private static final Logger log = Logger
+			.getLogger(ApplicationException.class.getName());
+
+	public ApplicationException() {
+		doLog();
+	}
+
 	public ApplicationException(String message) {
 		super(message);
+		doLog();
 	}
-	
+
 	public ApplicationException(Throwable cause) {
 		super(cause);
+		doLog();
 	}
-	
+
 	public ApplicationException(String message, Throwable cause) {
 		super(message, cause);
+		doLog();
+	}
+
+	private void doLog() {
+		ApplicationException.log.warning(getMessage());
 	}
 }
