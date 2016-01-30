754186d213ae8355bab518b0a92e53a19d707c4f
Myroslav Rudnytskyi <myroslav.rudnytskyi@gmail.com>
2016-01-30 19:36:50 +0200

    Reverting unstable changes

--- src/main/java/intelligent/wiki/editor/sweble/SwebleWikiArticle.java
+++ src/main/java/intelligent/wiki/editor/sweble/SwebleWikiArticle.java
@@ -1,11 +1,11 @@
 /*
  * Copyright (C) 2014 Myroslav Rudnytskyi
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
@@ -16,11 +16,10 @@ package intelligent.wiki.editor.sweble;
 import intelligent.wiki.editor.bot.compiler.AST.CategoryDeclaration;
 import intelligent.wiki.editor.bot.compiler.AST.Content;
 import intelligent.wiki.editor.core.WikiArticle;
-import intelligent.wiki.editor.sweble.AST.*;
 import javafx.scene.control.TreeItem;
 import org.sweble.wikitext.engine.nodes.EngPage;
 import org.sweble.wikitext.engine.nodes.EngProcessedPage;
-import org.sweble.wikitext.parser.nodes.*;
+import org.sweble.wikitext.parser.nodes.WtNode;
 
 import java.util.Collections;
 import java.util.List;
@@ -36,7 +35,6 @@ import java.util.Objects;
  * @version 19.09.2015
  */
 public class SwebleWikiArticle implements WikiArticle {
-
 	private final EngPage content;
 
 	public SwebleWikiArticle(EngProcessedPage content) {
@@ -55,29 +53,8 @@ public class SwebleWikiArticle implements WikiArticle {
 	}
 
 	private TreeItem<Content> createTree(WtNode node) {
-		TreeItem<Content> tree;
-		if (node instanceof WtBold) {
-			tree = new TreeItem<>(new BoldSweble((WtBold) node));
-		} else if (node instanceof WtItalics) {
-			tree = new TreeItem<>(new ItalicsSweble((WtItalics) node));
-		} else if (node instanceof WtExternalLink) {
-			tree = new TreeItem<>(new ExternalLinkSweble((WtExternalLink) node));
-		} else if (node instanceof WtInternalLink) {
-			tree = new TreeItem<>(new InternalLinkSweble((WtInternalLink) node));
-		} else if (node instanceof WtText) {
-			tree = new TreeItem<>(new TextSweble((WtText) node));
-		} else if (node instanceof WtSection) {
-			tree = new TreeItem<>(new SectionSweble((WtSection) node));
-//		} else if (node instanceof WtParagraph) {
-//			tree = new TreeItem<>();
-//		} else if (node instanceof WtOrderedList) {
-//			tree = new TreeItem<>();
-//		} else if (node instanceof WtUnorderedList) {
-//			tree = new TreeItem<>();
-		} else {
-			tree = new TreeItem<>(new Node(node));
-			node.stream().forEach(current -> tree.getChildren().add(createTree(current)));
-		}
+		TreeItem<Content> tree = new TreeItem<>(new Node(node));
+		node.stream().forEach(current -> tree.getChildren().add(createTree(current)));
 		return tree;
 	}
 }
